f11b94a1fd6d813bfa1cfb67c2907ebaac10fbcb
VladyslavLysak <vlad.lysak.26@gmail.com>
2020-05-23 17:46:29 +0300

    Updates

---
+++ precache-manifest.2bfb49e18d0e9152e415d9965ff14780.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b360e556e328054e8c90caa49474cbd",
+    "url": "/vladyslavlysak.github.io/index.html"
+  },
+  {
+    "revision": "d825ce61e40f9ec4921a",
+    "url": "/vladyslavlysak.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dd0a2e9e6c6065d54108",
+    "url": "/vladyslavlysak.github.io/static/css/main.f02fa6b5.chunk.css"
+  },
+  {
+    "revision": "d825ce61e40f9ec4921a",
+    "url": "/vladyslavlysak.github.io/static/js/2.e0e22a47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vladyslavlysak.github.io/static/js/2.e0e22a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0a2e9e6c6065d54108",
+    "url": "/vladyslavlysak.github.io/static/js/main.f499b102.chunk.js"
+  },
+  {
+    "revision": "551a712a06919c1d6afd",
+    "url": "/vladyslavlysak.github.io/static/js/runtime-main.f3c823c2.js"
+  },
+  {
+    "revision": "51a6dbdc043fe125f2e3e0bfa4d620e5",
+    "url": "/vladyslavlysak.github.io/static/media/back.51a6dbdc.jpg"
+  },
+  {
+    "revision": "26da84a1bcfc5dc4d8277fe7da050309",
+    "url": "/vladyslavlysak.github.io/static/media/black.26da84a1.png"
+  },
+  {
+    "revision": "cbeb4afccf204dfd379b07b5b53af37b",
+    "url": "/vladyslavlysak.github.io/static/media/meCV.cbeb4afc.jpg"
+  }
+]);
